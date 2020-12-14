05d0bbca74be8782377547f015fc949e970d7a5a
pianoFF <fangfei1star@gmail.com>
2020-12-14 13:45:21 -0500

    commit tree b8ba6cfc7d0d3bb4d1b03d79b5c25b4d40b64849

--- Gemfile
+++ Gemfile
@@ -5,3 +5,18 @@ source "https://rubygems.org"
 
 gem 'rspec'
 gem 'pry'
+gem 'did_you_mean'
+gem 'etc'
+gem 'fileutils'
+gem "executable-hooks"
+gem "delegate"
+gem 'uri'
+gem 'timeout'
+gem 'zlib'
+gem 'stringio'
+gem 'date'
+gem 'ipaddr'
+gem 'openssl'
+gem 'cgi'
+gem 'strscan'
+gem 'forwardable'
--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,10 +1,13 @@
 def my_select(collection)
  # your code here!
- new_collect = Array.new 
- counter = 0
- while counter < collection.length
-  new_collect.push(yield collection[counter])
-  counter += 1
- end
- return new_collect
+  new_collect = Array.new 
+  counter = 0
+  while counter < collection.length
+    # new_collect.push(yield collection[counter])
+    if yield(collection[counter])
+      new_collect.push(collection[counter])
+    end
+    counter += 1
+  end
+  return new_collect
 end
